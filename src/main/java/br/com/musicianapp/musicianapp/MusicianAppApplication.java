213b5ec0c526e4204b9302762ae1c31f96aaeeb9
dogdyzero <dhlg_29@hotmail.com>
2019-08-19 19:48:52 -0300

    spring-boot-starter

---
+++ src/main/java/br/com/musicianapp/musicianapp/MusicianAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.musicianapp.musicianapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicianAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicianAppApplication.class, args);
+	}
+
+}
