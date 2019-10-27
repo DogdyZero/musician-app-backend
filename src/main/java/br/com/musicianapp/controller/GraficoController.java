fbaa90a604156a5de01accb4df67a6d9aaa9c894
dogdyzero <dhlg_29@hotmail.com>
2019-10-26 23:55:04 -0300

    grafico

--- src/main/java/br/com/musicianapp/controller/GraficoController.java
+++ src/main/java/br/com/musicianapp/controller/GraficoController.java
@@ -1,89 +1,45 @@
 package br.com.musicianapp.controller;
 
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.musicianapp.grafico.Datasets;
-import br.com.musicianapp.grafico.Grafico;
+import br.com.musicianapp.domain.EntidadeDominio;
+import br.com.musicianapp.grafico.GraficoPesquisa;
+import br.com.musicianapp.impl.ConsultasPadrao;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/grafico")
 public class GraficoController {
 	
-	@PersistenceContext
-	private EntityManager em;
+	@Autowired
+	private Facade facade;
+	
+	@Autowired
+	private GraficoPesquisa grafico;
 	
 	@RequestMapping
-	public Object gerarGrafico() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("select extract(month from ped_data) as mes, sum(ped_valor)"); 
-		sb.append("from pedido");
-		sb.append("where ped_data>='2019-01-01' and ped_data<'2019-12-12'");
-		sb.append("group by mes");
-		
-		List<Object[]> result =  em.createQuery("select extract(month from data) , sum(total) from Pedido group by extract(month from data)").getResultList();
-		List<String> meses  =  new ArrayList<String>();
-		List<Double> valores = new ArrayList<Double>();
-		
-		for(Object[] obj : result) {
-			int mes = (int)obj[0];
-			String mesString = String.valueOf(mes);
-			meses.add(mesString);
-			double valor = (double) obj[1];
-			valores.add(valor);
-		}
-		
-		Grafico grafico = new Grafico();
-		Datasets dataSet1 = new Datasets();
-//		
-		dataSet1.setBorderColor("#1E88E5");
-		dataSet1.setFill(false);
-		dataSet1.setLabel("Valores");
-//		List<Integer> dadosTbl1 = new ArrayList<Integer>();
-//		dadosTbl1.add(2000);
-//		dadosTbl1.add(1500);
-//		dadosTbl1.add(1900);
-		dataSet1.setData(valores);
-//		
-//		Datasets dataSet2 = new Datasets();
-//
-//		
-//		dataSet2.setBorderColor("#755555");
-//		dataSet2.setFill(false);
-//		dataSet2.setLabel("Teste2");
-//		List<Integer> dadosTbl2 = new ArrayList<Integer>();
-//		dadosTbl2.add(400);
-//		dadosTbl2.add(500);
-//		dadosTbl2.add(700);
-//		dataSet2.setData(dadosTbl2);
-//		
-		List<Datasets> data = new ArrayList<Datasets>();
-		data.add(dataSet1);
-//		data.add(dataSet2);
-//		
-//		
-		grafico.setDatasets(data);
-//		
-//		List<String> nomesCampos = new ArrayList<String>();
-//		
-//		nomesCampos.add("A");
-//		nomesCampos.add("B");
-//		nomesCampos.add("C");
-		grafico.setLabels(meses);
+	public Object gerarGrafico(@RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") Date dataInicio,
+			@RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") Date dataFinal) {
 
-		return grafico;
-	}
-	private List<Integer> getCount(){
-		List<Object> result =  em.createQuery("select count(*) from  pedido").getResultList();
-		result.forEach(p ->System.out.println(p));
+		if(dataInicio.before(dataFinal)) {
+			grafico.setDataInicial(dataInicio);
+			grafico.setDataFinal(dataFinal);
+			facade.setParametro(ConsultasPadrao.GRAFICO);
+			List<EntidadeDominio> resultado = facade.consultar(grafico);
+			if(resultado!=null) {
+				Object grafico = resultado.get(0);
+				return grafico;
+			}
+		}
+
 		return null;
 	}
 	
---
+++ src/main/java/br/com/musicianapp/daos/GraficoDao.java
@@ -0,0 +1,153 @@
+package br.com.musicianapp.daos;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Service;
+
+import br.com.musicianapp.domain.EntidadeDominio;
+import br.com.musicianapp.grafico.Datasets;
+import br.com.musicianapp.grafico.Grafico;
+import br.com.musicianapp.grafico.GraficoPesquisa;
+
+@Service
+public class GraficoDao extends AbstractDao {
+	@PersistenceContext
+	private EntityManager em;
+	
+
+	@Override
+	public List<EntidadeDominio> consultar(EntidadeDominio entidade) {
+		GraficoPesquisa pesquisa = (GraficoPesquisa)entidade;
+		
+//		String queryJoin="join pedido.carrinhoCompra as carrinhoCompra" + 
+//				"join carrinhoCompra.itemProduto as itemProduto " + 
+//				"join itemProduto.produto as produto " + 
+//				"join produto.grupoPrecificacao as grupoPrecificacao";
+		
+		
+		Query query = (Query) em.createQuery(""
+				+ "select to_char(pedido.data,'YYYY/MM') as data, "
+				+ "sum(grupoPrecificacao.custoCompra) as custo, "
+				+ "sum(itemProduto.valorProduto) as venda from Pedido as pedido "
+				+ "join pedido.carrinhoCompra as carrinhoCompra "
+				+ "join carrinhoCompra.itemProduto as itemProduto "
+				+ "join itemProduto.produto as produto "
+				+ "join produto.grupoPrecificacao as grupoPrecificacao "
+				+ "where pedido.data>= :dataInicio and pedido.data< :dataFinal "
+				+ "group by to_char(pedido.data,'YYYY/MM') "
+				+ "order by data");
+		
+		query.setParameter("dataInicio",pesquisa.getDataInicial());
+		query.setParameter("dataFinal",pesquisa.getDataFinal());
+
+		List<Object[]> result = query.getResultList();
+		
+		List<String> meses  =  new ArrayList<String>();
+		List<Double> valores = new ArrayList<Double>();
+		List<Double> valores2 = new ArrayList<Double>();
+
+		
+		for(Object[] obj : result) {
+			String mes = (String)obj[0];
+			double valor = (double) obj[1];
+			double valor2 = (double) obj[2];
+			String[] separador = mes.split("/");
+			meses.add(separador[1]+"/"+separador[0]);
+			valores.add(valor);
+			valores2.add(valor2);
+
+		}
+		/*
+		 * caso o resultado seja 1 unico mês cria uma query resultado diário
+		 */
+		if(meses.size()==1) {
+			
+			query = (Query) em.createQuery(""
+					+ "select pedido.data,  "
+					+ "sum(grupoPrecificacao.custoCompra) as custo, "
+					+ "sum(itemProduto.valorProduto) as venda from Pedido as pedido "
+					+ "join pedido.carrinhoCompra as carrinhoCompra "
+					+ "join carrinhoCompra.itemProduto as itemProduto "
+					+ "join itemProduto.produto as produto "
+					+ "join produto.grupoPrecificacao as grupoPrecificacao "
+					+ "where pedido.data>= :dataInicio and pedido.data< :dataFinal "
+					+ "group by pedido.data "
+					+ "order by pedido.data");
+			
+			query.setParameter("dataInicio",pesquisa.getDataInicial());
+			query.setParameter("dataFinal",pesquisa.getDataFinal());
+
+			result = query.getResultList();
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			meses = new ArrayList<String>();
+			valores = new ArrayList<Double>();
+			valores2 = new ArrayList<Double>();
+			for(Object[] obj : result) {
+				Date data = (Date)obj[0];
+				double valor = (double) obj[1];
+				double valor2 = (double) obj[2];
+				String dataString = sdf.format(data);
+				meses.add(dataString);
+				valores.add(valor);
+				valores2.add(valor2);
+			}
+			
+		}
+		
+		Grafico grafico = new Grafico();
+		Datasets dataSet1 = new Datasets();
+
+		dataSet1.setBorderColor("#1E88E5");
+		dataSet1.setFill(false);
+		dataSet1.setLabel("Custo");
+		dataSet1.setData(valores);
+
+		Datasets dataSet2 = new Datasets();
+
+		dataSet2.setBorderColor("#755555");
+		dataSet2.setFill(false);
+		dataSet2.setLabel("Vendas");
+
+		dataSet2.setData(valores2);
+
+		List<Datasets> data = new ArrayList<Datasets>();
+		data.add(dataSet1);
+		data.add(dataSet2);
+
+		grafico.setDatasets(data);
+
+		grafico.setLabels(meses);
+		
+		List<EntidadeDominio> entidades = new ArrayList<EntidadeDominio>();
+		entidades.add(grafico);
+		return entidades;
+	}
+
+	@Override
+	public EntidadeDominio salvar(EntidadeDominio entidade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EntidadeDominio alterar(EntidadeDominio entidade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	@Override
+	public void apagar(EntidadeDominio entidade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
