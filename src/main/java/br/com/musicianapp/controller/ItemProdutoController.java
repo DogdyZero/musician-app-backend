9d8d507b7d4a65e05aef92274610e7af0beff4f7
dogdyzero <dhlg_29@hotmail.com>
2019-11-03 19:20:47 -0300

    arrumar troca

--- src/main/java/br/com/musicianapp/controller/ItemProdutoController.java
+++ src/main/java/br/com/musicianapp/controller/ItemProdutoController.java
@@ -50,9 +50,8 @@ public class ItemProdutoController {
 			item.setId(id);
 			Resultado resultado = this.facade.alterar(item);
 			ItemProduto itemAlterado = (ItemProduto) resultado.getEntidades().get(0);
-			this.troca.setId(itemAlterado.getProduto().getId());
 			this.facade.setParametro(ConsultasPadrao.TROCA_USUARIO);
-			List<EntidadeDominio> entidades = facade.consultar(this.troca);
+			List<EntidadeDominio> entidades = facade.consultar(itemAlterado.getTroca());
 			Pessoa pessoa = (Pessoa) entidades.get(0);
 			
 			Set<Cupom> cupons = new HashSet<Cupom>();
